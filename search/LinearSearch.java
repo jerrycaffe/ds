a5664ac28451a9fd0c5a4f5129bf2b9d52f490eb
Mac <mac@Macs-MacBook-Pro.local>
2023-08-10 23:12:54 +0100

    working on linear search

--- search/LinearSearch.java
+++ search/LinearSearch.java
@@ -8,6 +8,13 @@ public class LinearSearch {
         return -1;
     }
     public static void main(String[] args) {
+        int[] values = {1,2,3,4,5,6,7,8,9};
+        if(search(values, 10) == -1){
+            System.out.println("Not found");
+        }
+        else {
+            
+        }
         System.out.println(search(new int[]{1,2,3,4,5,6,7,8,9}, 10));
     }
 }
