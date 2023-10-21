04999e8faa6d387d1f0e225026b30c40e30d1ed0
jerrycaffe <mac@Jerrycaffe.local>
2023-10-21 20:42:17 +0100

    added description to enqueue and dequeue

--- ds/Queues/Queues.java
+++ ds/Queues/Queues.java
@@ -10,6 +10,15 @@ public class Queues<T> {
         this.head = null;
     }
 
+    /**
+     * Create a new node using the value passed
+     * If the queue is empty, set head and tail to the newly created node
+     * otherwise set the next property of the tail to the newly created node
+     * increment the size of the queue by 1
+     * 
+     * @param data
+     * @return boolean
+     */
     public boolean add(T data) {
         QueueNode<T> newData = new QueueNode<>(data);
         if (this.head == null) {
@@ -29,6 +38,14 @@ public class Queues<T> {
         else
             return null;
     }
+
+    /**
+     * If the property of head and tail is empty, return null
+     * get thehead stored within a variable as a temporary head
+     * Check if head and tail are the same, this means the element to be removed is
+     * last, remove from both and set value as null
+     * Decrease the size
+     */
 
     public T poll() {
         if (this.head == null)
