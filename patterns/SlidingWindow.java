2ede14ac4961d987593d71211b9a9b4e8bff16e0
jerrycaffe <jerrycaffe@gmail.com>
2024-05-16 10:33:37 +0100

    solved max sub array with naive and sliding window

--- patterns/SlidingWindow.java
+++ patterns/SlidingWindow.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 class SlidingWindow {
     // Given an array, find the average of all contiguous subarrays of size K in it.
     // e.g avgSubArray([1,2,3,54,6,7,8,9,76], 4), = (1+2+3+54)/4
-    static double findAvgSubArrays(int[] elements, int k) {
+    static double naiveAvgSubArrays(int[] elements, int k) {
         // loop through all the elements
         // do another inner loop using the given size
         // sum all the elements within k
@@ -31,7 +31,7 @@ class SlidingWindow {
 
     }
 
-    static double slidingWindow(int[] elements, int k) {
+    static double windowAvgSubarr(int[] elements, int k) {
         double results = Double.NEGATIVE_INFINITY;
 
         int windowSum = 0;
@@ -39,19 +39,53 @@ class SlidingWindow {
         for (int i = 0; i < elements.length; i++) {
             windowSum += elements[i];
             if (i >= k - 1) {
-                double div =(double) windowSum / k;
+                double div = (double) windowSum / k;
                 results = div > results ? div : results;
-               
-                windowSum -= elements[windowStart]; 
-                 windowStart++;
+
+                windowSum -= elements[windowStart];
+                windowStart++;
             }
         }
         return results;
     }
 
+    static int naiveMaxSumSubArr(int[] arr, int k) {
+        int result = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length - k + 1; i++) {
+            int sum = 0;
+            for (int j = i; j < i + k; j++) {
+                sum += arr[j];
+            }
+            result = sum > result ? sum : result;
+        }
+        return result;
+    }
+
+    static int windowMaxAvgSubarr(int[] elements, int k) {
+
+        int windowSum = 0;
+        int windowStart = 0;
+        int result = Integer.MIN_VALUE;
+
+        for (int i = 0; i < elements.length; i++) {
+            windowSum += elements[i];
+
+            if (i >= k - 1) {
+                result = windowSum > result ? windowSum : result;
+
+                windowSum -= elements[windowStart];
+                windowStart++;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         int[] elements = { 1, 3, 2, 6, -1, 4, 1, 8, 2 };
-        System.out.println(findAvgSubArrays(elements, 5));
-        System.out.println(slidingWindow(elements, 5));
+        System.out.println(naiveAvgSubArrays(elements, 5));
+        System.out.println(windowAvgSubarr(elements, 5));
+        System.out.println(naiveMaxSumSubArr(elements, 5));
+        System.out.println(windowMaxAvgSubarr(elements, 5));
     }
 }
