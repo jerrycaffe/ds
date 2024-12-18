e9a42bbf8d74d7559e81c18770f9d0ef8ff17b6a
jerrycaffe <jerrycaffe@gmail.com>
2024-12-18 11:40:30 +0100

    worked on the real solution

--- src/main/java/com/patterns/FrequencyCounter.java
+++ src/main/java/com/patterns/FrequencyCounter.java
@@ -4,6 +4,7 @@ package com.patterns;
 import com.sun.jdi.IntegerValue;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class FrequencyCounter {
@@ -13,14 +14,35 @@ public class FrequencyCounter {
      * The frequency of the values must be the same
      */
 
-    public Boolean sameSquareNaive(List<Integer> firstArr, List<Integer> secondArr) {
-
+    public Boolean sameSquareNaive(List<Integer> firstArr, List<Integer> secondArray) {
+        List<Integer> secondArr = new ArrayList<>(secondArray);
         if (firstArr.size() != secondArr.size()) return false;
         for (Integer element : firstArr) {
             var squared = element * element;
             if (secondArr.contains(squared)) {
                 secondArr.remove(Integer.valueOf(squared));
             } else return false;
+        }
+        return true;
+    }
+
+    public Boolean sameSquared(List<Integer> firstArr, List<Integer> secondArr){
+        if (firstArr.size() != secondArr.size()) return false;
+        //iterate through first array,
+        //check if the square of the current element exists as key within the hashmap, if it does increment the value else use the square of current element as key,
+        // loop through the second array, check if the current element exist within the hashmap, if it does, check if the value is not zero. reduce the value by 1
+        //else return false
+        //return true at the end of the operations
+
+        HashMap<Integer, Integer> mappedElement = new HashMap<>();
+        for (Integer element: firstArr ){
+            Integer squared = element * element;
+            if(mappedElement.containsKey(squared)) mappedElement.put(squared, mappedElement.get(squared) + 1);
+            else mappedElement.put(squared, 1);
+        }
+        for(Integer element: secondArr){
+            if(mappedElement.containsKey(element) && mappedElement.get(element) != 0) mappedElement.put(element, mappedElement.get(element) -1);
+            else return false;
         }
         return true;
     }
--- src/test/java/com/patterns/FrequencyCounterTest.java
+++ src/test/java/com/patterns/FrequencyCounterTest.java
@@ -10,16 +10,35 @@ import static org.junit.jupiter.api.Assertions.*;
 class FrequencyCounterTest {
     FrequencyCounter frequencyCounter = new FrequencyCounter();
 
+
     @Test
     public void testSameSquaredShouldFailWithUnequalInput(){
-        var result = frequencyCounter.sameSquareNaive(List.of(1,3,4), List.of(1, 9, 16, 25));
-        assertFalse(result);
+        var firstArr = List.of(1,3,4);
+        var secondArr = List.of(1, 9, 16, 25);
+        var naiveResult = frequencyCounter.sameSquareNaive(firstArr, secondArr);
+        var realResult = frequencyCounter.sameSquared(firstArr, secondArr);
+        assertFalse(naiveResult);
+        assertFalse(realResult);
     }
+
 
     @Test
     public void testSameSquareShouldFailUnbalancedSquare(){
-        var result = frequencyCounter.sameSquareNaive(List.of(1,2,3), new ArrayList<>(List.of(1,4,6)));
-        assertFalse(result);
+        var firstArr = List.of(1,2,3);
+        var secondArr = new ArrayList<>(List.of(1,4,6));
+        var naiveResult = frequencyCounter.sameSquareNaive(firstArr, secondArr);
+        var realResult = frequencyCounter.sameSquared(firstArr, secondArr);
+        assertFalse(naiveResult);
+        assertFalse(realResult);
+    }
+    @Test
+    public void testShouldPassOnCorrectInput(){
+        var firstArr = List.of(2,3,6,8,8);
+        var secondArr = new ArrayList<>(List.of(64, 36, 4, 9, 64));
+        var naiveResult = frequencyCounter.sameSquareNaive(firstArr, secondArr);
+        var realResult = frequencyCounter.sameSquared(firstArr, secondArr);
+        assertTrue(naiveResult);
+        assertTrue(realResult);
     }
 
 }
